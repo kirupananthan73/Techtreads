28b948a04af5392e0bb0a427df4577da52e733a8
Kirupananthan <kirupananthan27@gmail.com>
2025-04-25 17:42:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert("heyy")
+
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNavbar()
+{
+    sidenav.style.left="0"
+    
+}
+
+function closeNavbar()
+{
+    sidenav.style.left="-60%"
+    
+}
